a4e595c1b5e33c323ec6791854f0eda58c2e98b9
riza.aycelebi <differentiate.function@gmail.com>
2024-07-17 11:10:23 +0300

    Add project files.

---
+++ PROJETAKIP/Models/DataContext/ProjeTakipDBContext.cs
@@ -0,0 +1,22 @@
+﻿
+using PROJETAKIP.Models.Personeller;
+using PROJETAKIP.Models.ProjeTakip;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PROJETAKIP.Models.DataContext
+{
+    public class ProjeTakipDBContext: DbContext
+    {
+        public ProjeTakipDBContext(): base("ProjeTakipDB")
+        {
+
+        }
+
+        public DbSet<PersonelBilgileri> PersonelBilgileris { get; set; }
+        public DbSet<PersonelProjeleri> PersonelProjeleris { get; set; }
+    }
+}
